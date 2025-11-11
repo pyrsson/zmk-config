aaafc62dfe7939038b45c616c3a4f48c56b87a17
pyrsson <55557230+pyrsson@users.noreply.github.com>
2025-11-11 08:36:06 +0100

    better builing

--- build.sh
+++ build.sh
@@ -7,8 +7,7 @@ set -exo pipefail
 if ! podman container exists zmk-build; then
   podman create -it \
     --security-opt label=disable \
-    --workdir /workspaces/zmk \
-    -v ../zmk:/workspaces/zmk \
+    --workdir /workspaces/zmk-config \
     -v "$PWD":/workspaces/zmk-config \
     -p 3000:3000 \
     --name zmk-build \
@@ -16,15 +15,16 @@ if ! podman container exists zmk-build; then
     /bin/bash
 
   podman start zmk-build
-  podman exec -itw /workspaces/zmk zmk-build bash -c 'test -d .west || west init -l app; west update'
+  podman exec -it zmk-build cp -R config base/config
+  podman exec -itw /workspaces/zmk-config/base zmk-build bash -c 'test -d .west || west init -l config; west update --fetch-opt=--filter=tree:0; west zephyr-export'
 fi
 
 podman start zmk-build
-podman exec -itw /workspaces/zmk zmk-build west config build.cmake-args -- "-DZMK_CONFIG=/workspaces/zmk-config/config -DZMK_EXTRA_MODULES=/workspaces/zmk-config"
+podman exec -itw /workspaces/zmk-config/base zmk-build west config build.cmake-args -- "-DZMK_CONFIG=/workspaces/zmk-config/base/config -DZMK_EXTRA_MODULES=/workspaces/zmk-config -DSHIELD=nice_view_adapter -DSHIELD=nice_view_battery"
 
 if [[ $1 == "reset" ]]; then
-  podman exec -itw /workspaces/zmk/app zmk-build west build -d build/settings_reset -b lotus58_ble_left -- -DSHIELD=settings_reset
-  cp ../zmk/app/build/settings_reset/zephyr/zmk.uf2 zmk-settings-reset.uf2
+  podman exec -itw /workspaces/zmk-config/base zmk-build west build -s zmk/app -d build/settings_reset -b lotus58_ble_left -- -DSHIELD=settings_reset
+  cp base/build/settings_reset/zephyr/zmk.uf2 zmk-settings-reset.uf2
   exit 0
 fi
 
@@ -37,14 +37,12 @@ if [[ -n $side ]]; then
   if [[ $side = "left" ]]; then
     left=1
   fi
-  podman exec -itw /workspaces/zmk/app zmk-build west build -d build/$side -b lotus58_ble_$side ${left:+-S studio-rpc-usb-uart} $@ -- -DSHIELD=nice_view_adapter -DSHIELD=nice_view
-  cp ../zmk/app/build/$side/zephyr/zmk.uf2 zmk-$side.uf2
+  podman exec -itw /workspaces/zmk-config/base zmk-build bash -c "west zephyr-export; west build -s zmk/app -d build/$side -b lotus58_ble_$side ${left:+-S studio-rpc-usb-uart}"
+  cp base/build/$side/zephyr/zmk.uf2 zmk-$side.uf2
 else
-  podman exec -itw /workspaces/zmk/app zmk-build \
-    west build -d build/left -b lotus58_ble_left -S studio-rpc-usb-uart $@ -- -DSHIELD=nice_view_adapter -DSHIELD=nice_view
-  cp ../zmk/app/build/left/zephyr/zmk.uf2 zmk-left.uf2
+  podman exec -itw /workspaces/zmk-config/base zmk-build west build -s zmk/app -d build/left -b lotus58_ble_left -S studio-rpc-usb-uart $@
+  cp base/build/left/zephyr/zmk.uf2 zmk-left.uf2
 
-  podman exec -itw /workspaces/zmk/app zmk-build \
-    west build -d build/right -b lotus58_ble_right $@ -- -DSHIELD=nice_view_adapter -DSHIELD=nice_view
-  cp ../zmk/app/build/right/zephyr/zmk.uf2 zmk-right.uf2
+  podman exec -itw /workspaces/zmk-config/base zmk-build west build -s zmk/app -d build/right -b lotus58_ble_right $@
+  cp base/build/right/zephyr/zmk.uf2 zmk-right.uf2
 fi
